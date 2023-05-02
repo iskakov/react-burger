6df8be7043a137503b24ea6d3a9a3a45a2802b0f
Alex Iskakov <aaiskakov@phoenixit.ru>
2023-05-02 16:32:35 +0500

    fix

--- src/components/modal-overvlay/modal-overlay.jsx
+++ src/components/modal-overvlay/modal-overlay.jsx
@@ -3,8 +3,18 @@ import styles from './modal-overlay.module.css';
 import PropTypes from 'prop-types';
 
 const ModalOverlay = (props) => {
+
+  React.useEffect(() => {
+    document.getElementById('mainOverlay').focus()
+  })
+
+  const onKeyPress = (e) => {
+    if (e.key === 'Escape') {
+      props.onClose()
+    }
+  }
   return (
-    <section className={styles.main} onClick={props.onClose}>
+    <section id='mainOverlay' tabIndex="0" onKeyDown={onKeyPress} className={styles.main} onClick={props.onClose}>
       {props.children}
     </section>
   );
